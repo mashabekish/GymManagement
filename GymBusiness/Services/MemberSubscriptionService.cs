9d29dc530c9cb56a7401281483138168b29dfcfc
Maria Bekish <masha.bekish@gmail.com>
2023-12-21 12:24:20 +0300

    Add service methods

--- GymBusiness/Services/MemberSubscriptionService.cs
+++ GymBusiness/Services/MemberSubscriptionService.cs
@@ -2,5 +2,36 @@
 {
     public class MemberSubscriptionService
     {
+        private readonly MemberSubscriptionRepository _repository;
+
+        public MemberSubscriptionService(MemberSubscriptionRepository memberSubscriptionRepository)
+        {
+            _repository = memberSubscriptionRepository;
+        }
+
+        public MemberSubscription CreateAsync(MemberSubscription subscription)
+        {
+            return _repository.CreateAsync(subscription);
+        }
+
+        public MemberSubscription ViewAsync(int id)
+        {
+            return _repository.ViewAsync(id);
+        }
+
+        public IList<MemberSubscription> ListAsync()
+        {
+            return _repository.ListAsync();
+        }
+
+        public MemberSubscription UpdateAsync(MemberSubscription subscription)
+        {
+            return _repository.UpdateAsync(subscription);
+        }
+
+        public MemberSubscription DeleteAsync(int id)
+        {
+            return _repository.DeleteAsync(member);
+        }
     }
 }
--- GymBusiness/Services/SubscriptionService.cs
+++ GymBusiness/Services/SubscriptionService.cs
@@ -2,5 +2,36 @@
 {
     public class SubscriptionService
     {
+        private readonly SubscriptionRepository _repository;
+
+        public SubscriptionService(SubscriptionRepository subscriptionRepository)
+        {
+            _repository = subscriptionRepository;
+        }
+
+        public Subscription CreateAsync(Subscription subscription)
+        {
+            return _repository.CreateAsync(subscription);
+        }
+
+        public Subscription ViewAsync(int id)
+        {
+            return _repository.ViewAsync(id);
+        }
+
+        public IList<Subscription> ListAsync()
+        {
+            return _repository.ListAsync();
+        }
+
+        public Subscription UpdateAsync(Subscription subscription)
+        {
+            return _repository.UpdateAsync(subscription);
+        }
+
+        public Subscription DeleteAsync(int id)
+        {
+            return _repository.DeleteAsync(member);
+        }
     }
 }
