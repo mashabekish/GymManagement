c3681b83d5d45555215a3d727746abeaf431c0f1
Silkou Maxim <maxim.silkou.2002@gmail.com>
2023-12-21 15:09:59 +0300

    add edit

--- GymWebApp/Models/Member/MemberDetailsViewModel.cs
+++ GymWebApp/Models/Member/MemberDetailsViewModel.cs
@@ -1,9 +1,8 @@
-﻿using GymDomain.Entities;
-using GymWebApp.Models.MemberSubscription;
+﻿using GymWebApp.Models.MemberSubscription;
 
 namespace GymWebApp.Models.Member
 {
-    public class MemberViewModel
+    public class MemberDetailsViewModel
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
@@ -11,5 +10,8 @@ namespace GymWebApp.Models.Member
         public DateTime Birthday { get; set; }
         public string IdCardNumber { get; set; }
         public string Email { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public bool IsDeleted { get; set; }
+        public List<MemberSubscriptionViewModel> MemberSubscriptions { get; set; }
     }
 }
---
+++ GymWebApp/Models/MemberSubscription/MemberSubscriptionViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GymWebApp.Models.MemberSubscription
+{
+    public class MemberSubscriptionViewModel
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int RemainingSessions { get; set; }
+    }
+}
