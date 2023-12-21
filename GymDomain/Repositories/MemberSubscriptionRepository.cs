eb0cea885d1397ce1552dc63c33963e81bb634b7
Alex Khartaniuk <S200823@sggw.edu.pl>
2023-12-21 11:13:09 +0100

    Changed Interfaces

---
+++ GymDomain/Repositories/MemberSubscriptionRepository.cs
@@ -0,0 +1,37 @@
+﻿using GymDomain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymDomain.Repositories
+{
+    internal class MemberSubscriptionRepository : IMemberSubscriptionRepository
+    {
+        public Task<MemberSubscription> AddMemberSubAsync(MemberSubscription memberSub)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MemberSubscription> DeleteMemberSubAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<MemberSubscription>> ListAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MemberSubscription> UpdateMemberSubAsync(MemberSubscription memberSub)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MemberSubscription> ViewAsync(int code)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
