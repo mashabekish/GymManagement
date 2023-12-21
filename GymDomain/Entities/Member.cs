bee5fd83786e67c4905601b48ffa68712b49e738
Maria Bekish <masha.bekish@gmail.com>
2023-12-21 12:37:37 +0300

    Add DbContext

--- GymDomain/Entities/Member.cs
+++ GymDomain/Entities/Member.cs
@@ -1,15 +1,28 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace GymDomain.Entities
 {
-    public class Memeber
+    public class Member
     {
         public int Id { get; set; }
+
+        [Required]
         public string FirstName { get; set; }
+
+        [Required]
         public string LastName { get; set; }
+
         public DateTime Birthday { get; set; }
+
         public Guid IdCardNumber { get; set; }
+
+        [Required]
         public string Email { get; set; }
+
         public DateTime RegistrationDate { get; set; }
+
         public bool IsDeleted { get; set; }
+
+        public List<MemberSubscription> Subscriptions { get; set; }
     }
 }
